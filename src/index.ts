53f91081bc2be145084e02414187eb3a3d7a1938
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-12-04 12:32:25 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const app = express();
 // app.options('*', cors(corsOptions))
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173/');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
