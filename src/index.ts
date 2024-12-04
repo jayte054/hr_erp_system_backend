29eede04271c814ffc99b1364827034807edf1f2
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-12-04 12:31:47 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -37,10 +37,14 @@ const app = express();
 // app.options('*', cors(corsOptions))
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://hr-erp-system-backend.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173/');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+    if (req.method === 'OPTIONS') {
+        res.status(204).end(); // Respond with success for preflight
+    } else {
+        next();
+    }
 });
 
 mongoDbConnection()
