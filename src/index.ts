23ed2f17894d86a4bb55c6ead1e290ca4c620c07
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-12-04 12:04:07 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://hr-erp-system-backend.onrender.com' }));
 
 // const corsOptions = {
 //   origin: (origin: any, callback: any) => {
